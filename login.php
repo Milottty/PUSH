bdd0e7233f187d1ada64ba2cda91066617ee7d5d
Milottty <milotymeri1@gmail.com>
2025-04-05 12:37:07 +0200

    LoginLogic

--- login.php
+++ login.php
@@ -2,20 +2,20 @@
 
  <div  class="w-50 mx-auto" style="padding-top: 100px;">
     <h1 class="mb-3">Please sign in</h1>
- <form>
-  <!-- Email input -->
+ <form action="loginLogic.php" method="POST">
+
   <div data-mdb-input-init class="form-outline mb-4">
-    <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
+    <input type="text" id="form2Example1" class="form-control"  name="username"/>
+    <label class="form-label" for="form2Example1">Username</label>
   </div>
 
   <!-- Password input -->
   <div data-mdb-input-init class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" />
+    <input type="password" id="form2Example2" class="form-control" name="password" />
     <label class="form-label" for="form2Example2">Password</label>
   </div>
   <!-- Submit button -->
-  <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+  <button  type="submit" name="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
 
   <!-- Register buttons -->
   <div class="text-center">
